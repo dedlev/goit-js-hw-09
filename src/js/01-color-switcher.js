a94740a4f87d2874e82189a068925ecffa997c89
dedlev <l.popchuk@gmail.com>
2024-04-01 13:56:05 -0500

    add 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,2 +1,25 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
-console.log('hello')
+let timerId = null;
+
+startBtn.addEventListener("click", () => {
+ 
+    startBtn.setAttribute('disabled', false); 
+
+    timerId = setInterval(() => {
+        const changeColor = getRandomHexColor();
+        body.style.backgroundColor = changeColor;
+
+  }, 1000);
+});
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+    startBtn.removeAttribute('disabled');
+});
+
+ function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
